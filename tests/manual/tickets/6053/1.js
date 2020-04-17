7e523f6d764b120b87ba097e892039b2f0ee777f
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-04-17 15:35:19 +0200

    Tests: Added a manual test for the issue.

---
+++ tests/manual/tickets/6053/1.js
@@ -0,0 +1,19 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/* globals console:false, document, window */
+
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
+
+ClassicEditor
+	.create( document.querySelector( '#editor' ), {
+		plugins: [ ArticlePluginSet ],
+		toolbar: [ 'undo', 'redo', 'link' ]
+	} )
+	.then( newEditor => {
+		window.editor = newEditor;
+	} )
+	.catch( err => console.error( err.stack ) );
